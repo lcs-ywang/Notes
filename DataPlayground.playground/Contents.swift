2ed64e8c83509adc56dbaadd8cc8323d39fe31a0
yining wang <ywang@lcs.on.ca>
2021-01-12 14:51:34 -0500

    Conversion

--- DataPlayground.playground/Contents.swift
+++ DataPlayground.playground/Contents.swift
@@ -17,3 +17,28 @@ UInt8.min
 //
 //...the first digit counts for 2
 
+func getBinaryRepresentation(of valueToconvert: Int ) -> String{
+
+    //Create a variable with the value of "valueToConvert"
+    //a variable can be changed once created
+    var decimalValueLeftToConvert = valueToconvert
+
+    //This createa an emplty string
+    //A string is just text, like "hello"
+    var binaryReperesentation = ""
+    // The abstraction we will use is a loop
+    //Our end condition is that the decimalValueLeftToConvert is equal to Zero
+    //So long as the Condition is true, the block of code surrounded by the { } brackets will be run reapidly
+    while decimalValueLeftToConvert > 0 {
+        // Get the next decimal binary digit
+        let nextBinaryDigit = decimalValueLeftToConvert % 2
+        //Add that new digit to the binary representation
+        //Swift is a SECURITY TYPED language
+        //It DOES NOT automatically sovernt data types
+        //So, to make the Int into s string, we need to specify this
+        binaryReperesentation = String(nextBinaryDigit) + binaryReperesentation
+        decimalValueLeftToConvert = decimalValueLeftToConvert / 2
+}
+
+return binaryReperesentation
+}
